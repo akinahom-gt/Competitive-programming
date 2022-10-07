e3559e128b2e2949a6217a8e97dfbd75d23e0ab9
akinahom-gt <108095899+akinahom-gt@users.noreply.github.com>
2022-10-08 02:52:15 +0400

    Create MinStack.py

---
+++ MinStack.py
@@ -0,0 +1,44 @@
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = []
+        self.min_val = []
+        
+    def push(self, val: int) -> None:
+        self.stack += [val]
+        if self.min_val:
+            self.min_val += [min(val, self.min_val[-1])]
+        else:
+            self.min_val += [val]
+
+    
+        
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack = self.stack[:-1]
+        self.min_val = self.min_val[:-1]
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.min_val[-1]
+
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
