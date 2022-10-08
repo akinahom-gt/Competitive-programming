8fa4ccebc31983290a7576c02796424c6ba8909e
akinahom-gt <108095899+akinahom-gt@users.noreply.github.com>
2022-10-09 01:38:03 +0400

    Create StringCompression.py

---
+++ StringCompression.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+        result, cnt = 0, 1
+        j = 0
+        for i in range(1, len(chars)+1): 
+            if i < len(chars) and chars[i] == chars[i-1]:
+                cnt += 1
+            else:
+                chars[j] = chars[i-1]
+                j += 1
+                if cnt == 1:
+                    continue
+                for ch in str(cnt):
+                    chars[j] = ch
+                    j += 1
+                cnt = 1
+        return j
+        
