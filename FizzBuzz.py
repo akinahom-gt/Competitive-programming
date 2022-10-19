b306d75f42b83607c9eced74ac32bc35d37bd479
akinahom-gt <108095899+akinahom-gt@users.noreply.github.com>
2022-10-19 20:15:10 +0400

    Create FizzBuzz.py

---
+++ FizzBuzz.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        answer=[]
+        for number in range(1,n+1):
+            if (number % 3 == 0) and (number % 5 == 0):
+                answer.append("FizzBuzz")
+            elif (number % 3 == 0):
+                answer.append("Fizz")
+            elif (number %5==0):
+                answer.append("Buzz")
+            else:
+                answer.append(str(number))
+                
+                
+        return answer
+        
+        
+Footer
+© 2022 GitHub, Inc.
